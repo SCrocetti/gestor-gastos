6fda3ca8cc537bc77c75dc3321c4a45b8dc77f17
Sofia Crocetti <sofia.17.crocetti@gmail.com>
2024-05-20 14:15:23 -0300

    FIX add the migration script to create the transacciones table

---
+++ DB/V10__Create_transacciones_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS transacciones (
+    id_transaccion SERIAL PRIMARY KEY,
+    descripcion VARCHAR(100),
+    id_egreso INTEGER REFERENCES movimientos(id_movimiento) , 
+    id_ingreso INTEGER REFERENCES movimientos(id_movimiento) 
+);
