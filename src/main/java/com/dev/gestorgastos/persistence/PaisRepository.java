a37f333c3b5359460cfa86410c430c3d64d6e096
Sofia Crocetti <sofia.17.crocetti@gmail.com>
2024-05-21 23:20:35 -0300

    FEAT add query methods for PaisCrudRepository -Implement the saving, deleting and getting methods on PaisRepository

--- src/main/java/com/dev/gestorgastos/persistence/PaisRepository.java
+++ src/main/java/com/dev/gestorgastos/persistence/PaisRepository.java
@@ -1,7 +1,23 @@
 package com.dev.gestorgastos.persistence;
 
 import com.dev.gestorgastos.persistence.crud.PaisCrudRepository;
+import com.dev.gestorgastos.persistence.entity.Pais;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public class PaisRepository {
+    @Autowired
     PaisCrudRepository paisCrudRepository;
+    public List<Pais> getAll() {
+        return paisCrudRepository.findAllByOrderByNombrePaisAsc();
+    }
+    public Pais save(Pais pais){
+        return paisCrudRepository.save(pais);
+    }
+    public void delete(Integer paisId){
+        paisCrudRepository.deleteById(paisId);
+    }
 }
--- src/main/java/com/dev/gestorgastos/persistence/crud/PaisCrudRepository.java
+++ src/main/java/com/dev/gestorgastos/persistence/crud/PaisCrudRepository.java
@@ -3,5 +3,8 @@ package com.dev.gestorgastos.persistence.crud;
 import com.dev.gestorgastos.persistence.entity.Pais;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PaisCrudRepository extends CrudRepository<Pais,Integer> {
+import java.util.List;
+
+public interface PaisCrudRepository extends CrudRepository<Pais,Integer> {    // query method.
+    List<Pais> findAllByOrderByNombrePaisAsc();
 }
