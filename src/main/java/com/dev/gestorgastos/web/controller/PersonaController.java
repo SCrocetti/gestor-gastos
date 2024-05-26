31b36ac9e05b8e36ffcea915025bacfe01bed1ab
Sofia Crocetti <sofia.17.crocetti@gmail.com>
2024-05-26 14:08:44 -0300

    FEAT add swagger to the PersonaController -Still cant access swaggerui

--- src/main/java/com/dev/gestorgastos/web/controller/PersonaController.java
+++ src/main/java/com/dev/gestorgastos/web/controller/PersonaController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import io.swagger.annotations.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,24 +19,41 @@ public class PersonaController {
     private PersonaService personaService;
 
     @GetMapping("/id/{id}")
-    public ResponseEntity<PersonaDto> getByIdPersona(@PathVariable("id") Integer idPersona) {
+    @ApiOperation("Search a persona with an id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Persona not found")
+    })
+    public ResponseEntity<PersonaDto> getByIdPersona(@ApiParam(value ="Id of the persona",required = true,example = "7") @PathVariable("id")  Integer idPersona) {
         return personaService.getByIdPersona(idPersona)
                 .map(persona-> new ResponseEntity<PersonaDto>(persona, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/documento/{documento}")
-    public ResponseEntity<PersonaDto>  getByNumeroDocumento(@PathVariable("documento") String numeroDocumento) {
+    @ApiOperation("Search a persona with a documento")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Persona not found")
+    })
+    public ResponseEntity<PersonaDto>  getByNumeroDocumento(@ApiParam(value ="Documento of the persona",required = true,example = "666789") @PathVariable("documento") String numeroDocumento) {
         return personaService.getByNumeroDocumento(numeroDocumento)
                 .map(persona-> new ResponseEntity<PersonaDto>(persona, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
     @GetMapping("/all")
+    @ApiOperation("Get all personas data")
+    @ApiResponse(code=200, message = "OK")
     public ResponseEntity<List<PersonaDto>> getAll() {
         return new ResponseEntity<> (personaService.getAll(), HttpStatus.OK);
     }
 
     @PostMapping("/save")
+    @ApiOperation("Saves a new persona")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 409, message = "Conflict - Persona with the same numeroDocumento already exists")
+    })
     public ResponseEntity<PersonaDto> save(@RequestBody PersonaDto personaDto) {
         Optional<PersonaDto> existingPersona = personaService.getByNumeroDocumento(personaDto.getNumeroDocumento());
 
@@ -48,7 +65,12 @@ public class PersonaController {
         }
     }
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity delete(@PathVariable("id") Integer personaId) {
+    @ApiOperation("Deletes a persona by id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Persona not found")
+    })
+    public ResponseEntity delete(@ApiParam(value ="Id of the persona",required = true,example = "9") @PathVariable("id") Integer personaId) {
         return  new ResponseEntity(personaService.delete(personaId) ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 }
